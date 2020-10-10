b4c58ba049a19b8e9c6b98ef66003187ee23ef82
Michal Zygor <michal@winami.com>
2020-10-10 02:55:06 +0200

    004_exercise_4: step3 - small corrections

--- src/SpecialOffer.java
+++ src/SpecialOffer.java
@@ -1,3 +1,5 @@
+import javax.crypto.spec.PSource;
+
 public class SpecialOffer {
     String offerDescription;
     String dateStart;
@@ -11,17 +13,17 @@ public class SpecialOffer {
         dateStart = dateStartIn;
         dateEnd = dateEndIn;
         discount = discountIn;
-        product = new Product(productIn.name, productIn.description, productIn.price, productIn.category.nameCategory,
-                productIn.category.description);
+        product = new Product(productIn.name, productIn.description, productIn.price, productIn.category.nameCategory,productIn.category.description);
     }
 
     void showSpecialOffers(SpecialOffer specialOfferIn){
         System.out.println("Opis promocji: " + specialOfferIn.offerDescription + ", czas trwania od: "
                 + specialOfferIn.dateStart + " do " + specialOfferIn.dateEnd + ", przecenia od kwoty standardowej to "
                 + specialOfferIn.discount * 100 + "%");
-        System.out.println("Nazwa produkctu: " + specialOfferIn.product.name + ", cena: " + specialOfferIn.product.price * (1-specialOfferIn.discount ) + ", opis: "
-                + specialOfferIn.product.description + ", kategoria i opis: " + specialOfferIn.product.category.nameCategory
-                + " (" + specialOfferIn.product.category.description + ").");
+        System.out.print("Nazwa produkctu: " + specialOfferIn.product.name + ", cena: ");
+        System.out.printf( "%.2f", specialOfferIn.product.price * (1-specialOfferIn.discount ));
+        System.out.println(", opis: " + specialOfferIn.product.description + ", kategoria i opis: "
+                + specialOfferIn.product.category.nameCategory + " (" + specialOfferIn.product.category.description + ").");
 
     }
 
